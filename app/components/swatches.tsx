0198fbd4f36dab7917581a8b16ef63c375ff90fc
worker-201 <worker201@gmail.com>
2026-03-01 02:15:30 -0600

    Initial commit.

---
+++ app/components/swatches.tsx
@@ -0,0 +1,25 @@
+import colors from '@/public/data/colors.json';
+// import type { ColorSwatch } from '@/app/lib/definitions';
+
+
+export default function Swatches() {
+  // Check if colors is an array before mapping to avoid errors
+  if (!Array.isArray(colors)) {
+    return <p>No colors available.</p>;
+  }
+
+  return (
+    <div>
+      <h2>Filter by color:</h2>
+      <div className="flex gap-3">
+        {colors.map((color) => {
+          return (
+            <a href={`/?color=${color.value}`} key={color.value} className={`bg-${color.class} block w-6 h-6 border custom-swatch`} aria-label={`color ${color.name}`}>
+              &nbsp;
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  );
+};
---
+++ app/lib/definitions.ts
@@ -0,0 +1,5 @@
+export type ColorSwatch = {
+  name: string;
+  value: string;
+  class: string;
+};
