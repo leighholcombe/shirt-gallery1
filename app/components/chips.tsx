2ed096d8f8235f1a60ab45e5259732935b53f26f
worker-201 <worker201@gmail.com>
2026-03-01 23:50:02 -0600

    Added flex-wrap to chips

--- app/components/chips.tsx
+++ app/components/chips.tsx
@@ -9,7 +9,7 @@ export default function Chips() {
   return (
     <div>
       <h2>Filter by category:</h2>
-      <div className="flex gap-3">
+      <div className="flex gap-3 flex-wrap">
         {tags.map((chip) => {
           return (
             <a href={`/?cat=${chip.value}`} key={chip.value} className="bg-sky-300 text-neutral-800 rounded-full block py-2 px-4 whitespace-nowrap hover:bg-indigo-300">
