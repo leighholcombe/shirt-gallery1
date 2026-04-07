b32fd7e80f0c0ed1aa8928193aecd6a859c8e511
worker-201 <worker201@gmail.com>
2026-04-07 02:38:29 -0500

    Added more shirts and categories and images.

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -3,7 +3,7 @@ import { Shirt } from "../lib/definitions";
 
 export default function Card({ id, image, description, color, sleeves, tag_array, archived }: Shirt) {
   return (
-    <div className="border-2 p-4 relative w-1/4 aspect-square">
+    <div className="border-2 p-4 relative w-1/5 aspect-square">
       <Image
         src={image}
         fill={true}
